07575b910259ed3b1979c7012588b13d12295f6b
kusumakrishna <kusumakrishna524@gmail.com>
2022-12-27 23:21:03 +0530

    first commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MongoURI: ("mongodb+srv://kusumakrishna:kusumakrishna@cluster0.4pirw4v.mongodb.net/?retryWrites=true&w=majority")
+    // mongodb+srv://kusumakrishna:kusumakrishna@cluster0.4pirw4v.mongodb.net/?retryWrites=true&w=majority")
+}
